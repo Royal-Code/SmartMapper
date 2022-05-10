05c3b538c859621226e8ede2638d2ad1d14db98b
Glauco Knihs <eglauko@hotmail.com>
2022-05-09 23:16:27 -0300

    mapping

---
+++ SmartMapper/RoyalCode.SmartMapper/Configuring/Adapters/IAdapterOptionsBuilder.cs
@@ -0,0 +1,58 @@
+﻿
+using System.Linq.Expressions;
+
+namespace RoyalCode.SmartMapper.Configuring.Adapters;
+
+public interface IAdapterOptionsBuilder
+{
+    IAdapterOptionsBuilder Configure<TSource, TTarget>(Action<IAdapterOptionsBuilder<TSource, TTarget>> configure);
+
+    IAdapterOptionsBuilder<TSource, TTarget> Configure<TSource, TTarget>();
+}
+
+
+public interface IAdapterOptionsBuilder<TSource, TTarget>
+{
+    IAdapterPropertyOptionsBuilder<TSource, TTarget, TProperty> Map<TProperty>(Expression<Func<TSource, TProperty>> propertySelection);
+
+    IAdapterPropertyOptionsBuilder<TSource, TTarget, TProperty> Map<TProperty>(string propertyName);
+}
+
+public interface IAdapterPropertyOptionsBuilder<TSource, TTarget, TSourceProperty>
+{
+    IAdapterPropertyOptionsBuilder<TSource, TTarget, TSourceProperty, TTargetProperty> To<TTargetProperty>(Expression<Func<TTarget, TTargetProperty>> propertySelection);
+
+    IAdapterPropertyOptionsBuilder<TSource, TTarget, TSourceProperty, TTargetProperty> To<TTargetProperty>(string propertyName);
+}
+
+public interface IAdapterPropertyOptionsBuilder<TSource, TTarget, TSourceProperty, TTargetProperty>
+{
+
+}
+
+public static class ConfigureSample
+{
+    public static void Configure(IAdapterOptionsBuilder builder)
+    {
+        builder.Configure<MyDto, MyEntity>(b =>
+        {
+
+        });
+
+    }
+
+
+    public class MyEntity
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+    }
+
+    public class MyDto
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
