db00d2de07fb3a91e9f5c23b9e8a0eb9b0ef3d01
eglauko <eglauko@hotmail.com>
2022-06-15 18:18:20 -0300

    review of diagrams e more

--- SmartMapper/RoyalCode.SmartMapper/Resolvers/AssignmentStrategies/ConvertStrategy.cs
+++ SmartMapper/RoyalCode.SmartMapper/Resolvers/AssignmentStrategies/ConvertStrategy.cs
@@ -1,18 +1,24 @@
 ﻿
 using RoyalCode.SmartMapper.Resolvers.Assigners;
 using System.Diagnostics.CodeAnalysis;
+using RoyalCode.SmartMapper.Resolvers.Converters;
 
 namespace RoyalCode.SmartMapper.Resolvers.AssignmentStrategies;
 
 public class ConvertStrategy : IAssignmentStrategy
 {
+    public static readonly List<IValueConverter> Converters = new();
+    
     public bool CanResolve(
         Type sourceType,
         Type targetType,
         [NotNullWhen(true)] out IValueAssigner? valueAssigner)
     {
+        valueAssigner = null;
+        if (targetType.IsAssignableFrom(sourceType))
+            return false;
 
-
+        
 
         throw new NotImplementedException();
     }
---
+++ SmartMapper/RoyalCode.SmartMapper/Resolvers/Converters/IValueConverter.cs
@@ -0,0 +1,8 @@
+namespace RoyalCode.SmartMapper.Resolvers.Converters;
+
+public interface IValueConverter { }
+
+public interface IValueConverter<TSource, TTarget>
+{
+    
+}
