17ca6786cc8e39ae6c846181535c0ee9bd1142eb
Glauco Knihs <eglauko@hotmail.com>
2022-08-05 23:44:01 -0300

    Iniciando testes com AdapterPropertyOptionsBuilder. Mapeamento propriedade a propriedade.

--- SmartMapper/RoyalCode.SmartMapper.Adapters.Tests/Builders/AdapterPropertyOptionsBuilderTests.cs
+++ SmartMapper/RoyalCode.SmartMapper.Adapters.Tests/Builders/AdapterPropertyOptionsBuilderTests.cs
@@ -1,13 +1,54 @@
-﻿using System;
+﻿using FluentAssertions;
+using RoyalCode.SmartMapper.Exceptions;
+using RoyalCode.SmartMapper.Infrastructure.Adapters;
+using RoyalCode.SmartMapper.Infrastructure.Adapters.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace RoyalCode.SmartMapper.Adapters.Tests.Builders;
 
-public class AdapterPropertyOptionsBuilderTests
+public class AdapterProTpertyOptionsBuilderTests
 {
+    [Fact]
+    public void To_Must_Throw_When_SelectorIsNotAProperty()
+    {
+        var adapterOptions = new AdapterOptions(typeof(Foo), typeof(Bar));
+        var propertyOptions = adapterOptions.GetPropertyOptions(typeof(Foo).GetProperty("Value")!);
+        var builder = new AdapterPropertyOptionsBuilder<Foo, Bar, string>(adapterOptions, propertyOptions);
 
-    
+        var act = () => builder.To<Delegate>(x => x.DoSomething);
+
+        act.Should().Throw<InvalidPropertySelectorException>();
+    }
+
+    [Fact]
+    public void To_Must_ReturnTheOptionsBuilder_When_SelectorAValidProperty()
+    {
+        var adapterOptions = new AdapterOptions(typeof(Foo), typeof(Bar));
+        var propertyOptions = adapterOptions.GetPropertyOptions(typeof(Foo).GetProperty("Value")!);
+        var builder = new AdapterPropertyOptionsBuilder<Foo, Bar, string>(adapterOptions, propertyOptions);
+
+        var nextBuilder = builder.To(x => x.OtherValue);
+
+        nextBuilder.Should().NotBeNull();
+    }
+
+    private class Foo
+    {
+        public string Value { get; set; }
+    }
+
+    private class Bar
+    {
+        public string OtherValue { get; set; }
+
+        public string DoSomething()
+        {
+            return string.Empty;
+        }
+    }
 }
